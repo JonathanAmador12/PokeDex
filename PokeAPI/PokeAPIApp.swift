794d06e7e9d9057dd20c50d301e2f6e8727d0c8f
Jonathan A <jamadorh91@gmail.com>
2022-09-01 22:21:55 -0500

    Initial Commit

---
+++ PokeAPI/PokeAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeAPIApp.swift
+//  PokeAPI
+//
+//  Created by JonathanA on 01/09/22.
+//
+
+import SwiftUI
+
+@main
+struct PokeAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
