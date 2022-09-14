d040ebfa5bad2ba1724ee2aed63240292d6d1312
Jonathan A <jamadorh91@gmail.com>
2022-09-14 14:27:09 -0500

    we added a loader

--- PokeAPI/View/PokeDex.swift
+++ PokeAPI/View/PokeDex.swift
@@ -18,51 +18,59 @@ struct PokeDex: View {
     ]
     
     var body: some View {
-        
-        VStack(spacing: 0) {
-            HStack {
-                Text("PokeDex")
+        VStack{
+            if pokemonVM.isLoading {
+                ProgressView("Cargando...")
+                    .scaleEffect(2)
                     .foregroundColor(.white)
-                    .font(.title2)
-                    .fontWeight(.bold)
-                Spacer()
-            }
-            .padding()
-            .background(Color("primary"))
-            ScrollView {
-                if let pokemon = pokemonVM.pokemonPaginador{
-                    LazyVGrid(columns: formaGrid, spacing: 8) {
-                        ForEach(pokemon.results, id: \.self) { pokemon in
-                            VStack(spacing: 30) {
-                                
-                                KFImage(pokemonVM.getPokemonImage(name: pokemon.name))
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 100, height: 100)
-                                    .foregroundColor(.red)
-                                Text("\(pokemon.name)")
+                
+            }else{
+                VStack(spacing: 0) {
+                    HStack {
+                        Text("PokeDex")
+                            .foregroundColor(.white)
+                            .font(.title2)
+                            .fontWeight(.bold)
+                        Spacer()
+                    }
+                    .padding()
+                    .background(Color("primary"))
+                    ScrollView {
+                        if let pokemon = pokemonVM.pokemonList{
+                            LazyVGrid(columns: formaGrid, spacing: 8) {
+                                ForEach(pokemon, id: \.self) { pokemon in
+                                    VStack(spacing: 30) {
+                                        KFImage(pokemonVM.getPokemonImage(name: pokemon.name))
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fit)
+                                            .frame(width: 100, height: 100)
+                                            .foregroundColor(.red)
+                                        Text("\(pokemon.name)")
+                                    }
+                                    .padding(.bottom, 30)
+                                    .onAppear{
+                                        pokemonVM.loadMoreData(name: pokemon.name)
+                                    }
+                                    
+                                }
                             }
-                            .padding(.bottom, 30)
                             
                         }
                     }
-                    
+                    .frame(
+                        minWidth: 0,
+                        maxWidth: .infinity,
+                        minHeight: 0,
+                        maxHeight: .infinity
+                    )
+                    .background(Color("background"), ignoresSafeAreaEdges: .bottom)
                 }
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
             }
-            .onAppear{
-                pokemonVM.getPokemon()
-                
-            }
-            .frame(
-                minWidth: 0,
-                maxWidth: .infinity,
-                minHeight: 0,
-                maxHeight: .infinity
-            )
-            .background(Color("background"), ignoresSafeAreaEdges: .bottom)
         }
-        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
-        
+        .onAppear{
+            pokemonVM.getPokemon()
+        }
     }
 }
 
