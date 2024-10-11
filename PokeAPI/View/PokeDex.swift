d431d5233b3939240a040c425fe42a90291ed3d0
Jonathan A <jamadorh91@gmail.com>
2024-10-11 10:31:43 -0600

    Update: ProgessView - ProgessView was update

--- PokeAPI/View/PokeDex.swift
+++ PokeAPI/View/PokeDex.swift
@@ -16,77 +16,78 @@ struct PokeDex: View {
         GridItem(.flexible()),
         GridItem(.flexible())
     ]
-    
     @State var pokemon : Pokemon?
     
     var body: some View {
         VStack{
-            if pokemonVM.isLoading {
-                ProgressView("Cargando...")
-                    .scaleEffect(2)
-                    .foregroundColor(.white)
-                
-            }else{
-                ZStack {
-                    // first layer
-                    VStack(spacing: 0) {
-                        HStack {
-                            Text("PokeDex")
-                                .foregroundColor(.white)
-                                .font(.title2)
-                                .fontWeight(.bold)
-                            Spacer()
-                        }
-                        .padding()
-                        .background(Color("primary"))
-                        ScrollView {
-                            
-                            LazyVGrid(columns: formaGrid, spacing: 8) {
-                                ForEach(pokemonVM.pokemonList, id: \.self) { pokemon in
-                                    VStack(spacing: 30) {
-                                        KFImage(pokemonVM.getPokemonImage(name: pokemon.name))
-                                            .resizable()
-                                            .aspectRatio(contentMode: .fit)
-                                            .frame(width: 100, height: 100)
-                                            .foregroundColor(.red)
-                                        Text("\(pokemon.name)")
-                                    }
-                                    .padding(.bottom, 30)
-                                    
-                                    .onTapGesture(count: 1) {
-                                        isActive = true
-                                        self.pokemon = pokemonVM.PokemonDetails(name: pokemon.name)
-                                    }
-                                    .onAppear{
-                                        pokemonVM.loadMoreData(name: pokemon.name)
-                                    }
+            
+            ZStack {
+                // first layer
+                VStack(spacing: 0) {
+                    HStack {
+                        Text("PokeDex")
+                            .foregroundColor(.white)
+                            .font(.title2)
+                            .fontWeight(.bold)
+                        Spacer()
+                    }
+                    .padding()
+                    .background(Color("primary"))
+                    ScrollView {
+                        LazyVGrid(columns: formaGrid, spacing: 8) {
+                            ForEach(pokemonVM.pokemonList, id: \.self) { pokemon in
+                                VStack(spacing: 30) {
+                                    KFImage(pokemonVM.getPokemonImage(name: pokemon.name))
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 100, height: 100)
+                                        .foregroundColor(.red)
+                                    Text("\(pokemon.name)")
+                                }
+                                .padding(.bottom, 30)
+                                
+                                .onTapGesture(count: 1) {
+                                    isActive = true
+                                    self.pokemon = pokemonVM.PokemonDetails(name: pokemon.name)
+                                }
+                                .onAppear{
+                                    pokemonVM.loadMoreData(name: pokemon.name)
                                 }
                             }
                         }
-                        .frame(
-                            minWidth: 0,
-                            maxWidth: .infinity,
-                            minHeight: 0,
-                            maxHeight: .infinity
+                    }
+                    .frame(
+                        minWidth: 0,
+                        maxWidth: .infinity,
+                        minHeight: 0,
+                        maxHeight: .infinity
+                    )
+                    .background(Color("background"), ignoresSafeAreaEdges: .bottom)
+                }
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                
+                if pokemonVM.isLoading {
+                    ProgressView("Cargando...")
+                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                        .scaleEffect(4)
+                        .foregroundColor(.black)
+                        .font(.title3)
+                }
+                
+                // second layer
+                if isActive {
+                    if let detailPokemon = pokemon {
+                        Details(
+                            isActive: $isActive,
+                            pokemon: detailPokemon
                         )
-                        .background(Color("background"), ignoresSafeAreaEdges: .bottom)
+                            .environmentObject(pokemonVM)
                     }
-                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
-                    // second layer
-                    if isActive{
-                        if let detailPokemon = pokemon{
-                            Details(
-                                isActive: $isActive,
-                                pokemon: detailPokemon
-                            )
-                                .environmentObject(pokemonVM)
-                        }
-                      
-                    }
+                  
                 }
             }
         }
-        .onAppear{
+        .onAppear {
             pokemonVM.getPokemon()
         }
     }
